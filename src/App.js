02630bdce072281695c7279eb0ec7ac6c370e404
cherifa.informatique@gmail.com <cherifa.informatique@gmail.com>
2023-08-20 22:06:42 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './navbar';
 
 
 
+
 function App() {
   return (
     
--- src/navbar.js
+++ src/navbar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './navbar.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-  
+   
     <nav className="navbar">
       <div className="navbar-logo">
         <img src="/images/logo.png" alt="Logo" />
       </div>
       <ul className="navbar-nav">
-    
+      <li>
+              <Link to="/inscription">Inscription</Link>
+            </li>
+            <br></br>
         <li className="nav-item"><a href="/inscription">Inscription</a></li>
-        <li className="nav-item"><a href="signIn">Se Connecter</a></li>
-        <li className="nav-item"><a href="contact">Contact</a></li>
+        <li className="nav-item"><a href="/signIn">Se Connecter</a></li>
+        <li className="nav-item"><a href="/Contact">Contact</a></li>
       </ul>
     </nav>
-   
-   
+  
   );
 }
 export default Navbar;
